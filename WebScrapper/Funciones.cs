b57ce8862739b232eb0f796276103a926ea4ca35
Marco Carrillo <marco.37099p@gmail.com>
2023-03-15 19:10:00 -0600

    Agregar archivos de proyecto.

---
+++ WebScrapper/Funciones.cs
@@ -0,0 +1,34 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WebScrapper
+{
+    class Funciones
+    {
+        public static void GuardarLista(HtmlNodeCollection nodos)
+        {
+            string path = @"C:\Users\Marco Carrillo\Desktop\C#\WebScrapper\WebScrapper\lista.txt";
+            if (!File.Exists(path))
+            {
+                StreamWriter sw = File.CreateText(path);
+                foreach (var nodo in nodos)
+                {
+                    sw.WriteLine(nodo.InnerText);
+                }
+                sw.Dispose();
+            }
+            else
+            {
+                StreamWriter sw = new StreamWriter(path, false);
+                foreach (var nodo in nodos)
+                {
+                    sw.WriteLine(nodo.InnerText);
+                }
+                sw.Dispose();
+            }
+        }
+    }
+}
---
+++ WebScrapper/Program.cs
@@ -0,0 +1,76 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+
+namespace WebScrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("W E B  S C R A P P E R");
+            Console.WriteLine(@"
+                      #####
+     #    #######                    #######
+   ###          ####                ##      #####
+   #               ##              #             ###
+    #                #            #                ###
+    #                 #          #                   ##
+    #        ###      #         #                     #
+    #   ######  #     #         #        #####         #
+   ##   # ##### #     #         #       # ### #        ##
+   #   #   #### #     #         #       #######         #
+  ##   ### #####     #          ##       ######         #
+ ##       ###       ##            #                     #
+##                  #              ###                  #
+##                 #                  ##               ##
+ #              ###                     ####           #
+  ##         ###                           ############
+    #########
+
+");
+
+            /*
+             Crea una instancia de la clase HtmlWeb y usa el método Load() 
+             para cargar la página web que quieres scrapear:
+             */
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://github.com/M4rquinhos?tab=repositories");
+            /*
+             Utiliza la clase HtmlNode para buscar los elementos que deseas extraer de la página. 
+             Puedes hacerlo a través del método SelectNodes() pasándole un XPath que identifique los elementos 
+             que te interesan:
+             */
+            HtmlNodeCollection nodos = doc.DocumentNode.SelectNodes("//h3[@class='wb-break-all']//a");
+            /*
+             Recorre la colección de nodos y extrae la información que necesitas. 
+             Por ejemplo, si quieres extraer el texto de un nodo de texto:
+             */
+            Console.WriteLine("¿Quiere guardar la lista de repositorios? Si / No");
+            string opc = Console.ReadLine();
+            bool op = true;
+            try
+            {
+                if (!opc.ToLower().Equals("si"))
+                {
+                    op = false;
+                }
+
+                foreach (var nodo in nodos)
+                {
+                    Console.WriteLine(nodo.InnerText);
+                }
+
+                if (op)
+                {
+                    Funciones.GuardarLista(nodos);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
